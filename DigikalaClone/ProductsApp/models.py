a08c7247852b247a37e0c67743803810e1f59870
ggmasterqwe <navid.fida@yahoo.com>
2020-11-07 16:55:16 +0330

    feat: add DigiKalaProducts to save price and name of digikala

--- DigikalaClone/ProductsApp/models.py
+++ DigikalaClone/ProductsApp/models.py
@@ -7,8 +7,11 @@ User = get_user_model()
 
 class Product(models.Model):
     name = models.CharField(max_length=30)
-    description = models.CharField(max_length=40)
+    description = models.CharField(max_length=40, null=True, blank=True)
     price = models.BigIntegerField()
+
+    def __str__(self):
+        return f'{self.name}'
 
     def jsonify(self):
         return {
@@ -27,3 +30,8 @@ class Stock(models.Model):
     store = models.ForeignKey('Store', on_delete=models.CASCADE)
     product = models.ForeignKey('Product', on_delete=models.CASCADE)
     unit_in_stock = models.IntegerField(default=0)
+
+
+class DigiKalaProducts(models.Model):
+    name = models.CharField(max_length=50)
+    price = models.BigIntegerField(default=0)
--- DigikalaClone/root/celery.py
+++ DigikalaClone/root/celery.py
@@ -1,5 +1,6 @@
 
 from celery import Celery
+from celery.schedules import crontab
 import os
 
 # set the default Django settings module for the 'celery' program.
@@ -12,7 +13,15 @@ celery_app = Celery('root')
 # - namespace='CELERY' means all celery-related configuration keys
 #   should have a `CELERY_` prefix.
 celery_app.config_from_object('django.conf:settings', namespace='CELERY')
-
+celery_app.conf.timezone = 'Asia/Tehran'
 # Load task modules from all registered Django app configs.
 celery_app.autodiscover_tasks()
 
+# add periodic task for django celery-beat
+celery_app.conf.beat_schedule = {
+    'update-digikala': {
+        'task': 'ProductsApp.tasks.update_products',
+        'schedule': crontab(minute='*',hour='*/3'),
+        
+    },
+}
