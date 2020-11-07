bfab4ecd9fe76f0bb97db3a4cd0162dde4111858
ggmasterqwe <navid.fida@yahoo.com>
2020-11-07 19:45:00 +0330

    add: add admin for model

--- DigikalaClone/ProductsApp/admin.py
+++ DigikalaClone/ProductsApp/admin.py
@@ -2,4 +2,6 @@ from django.contrib import admin
 from .models import Product, DigiKalaProducts
 # Register your models here.
 admin.site.register(Product)
-admin.site.register(DigiKalaProducts)
+@admin.register(DigiKalaProducts)
+class DigiKalaProductsAdmin(admin.ModelAdmin):
+    list_display = ['name', 'price']
